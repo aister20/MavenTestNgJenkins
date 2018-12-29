320cf7d8b15355d061da25a25b6de147bef884e3
Andres <andreco06@hotmail.com>
2018-12-29 12:22:03 -0500

    second one

--- src/test/java/com/mavenTestNg/BaseClass.java
+++ src/test/java/com/mavenTestNg/BaseClass.java
@@ -1,5 +1,13 @@
 package com.mavenTestNg;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class BaseClass {
 
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "Drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+	}
 }
